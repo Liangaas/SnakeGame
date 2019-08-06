0832b81421c924adec4e09b087a557df878d4948
梁奕菡 <liangyihan@sunmi.com>
2019-08-06 20:01:22 +0800

    1. initSnakeGame

--- src/SnakeGame.js
+++ src/SnakeGame.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import styles from './SnakeGame.less';
+import './SnakeGame.less';
 
-class SnaekGame extends React.Component{
+class SnakeGame extends React.Component{
 	restartGame=()=> {}
 	pauseGame=()=> {}
 	render(){
 		return(
-			<div className={styles['game-container']}>
-				<div className={styles['game-info']}>
-					<div className={styles['game-opera']}>
+			<div className='game-container'>
+				<div className='game-info'>
+					<div className='game-opera'>
 						<p> 键盘操作：WSAD 上下左右</p>
 					</div>
-					<div className={styles['game-btn']}>
+					<div className='game-btn'>
 						<button onClick={()=>this.restartGame}>重新开始</button>
 						<button onClick={()=>this.pauseGame}>暂停游戏</button>
 					</div>
 				</div>
-				<div className={styles['game-background']}></div>
+				<div className='game-background'></div>
 			</div>
 		)
 	}
 }
 
-export default SnaekGame;
+export default SnakeGame;
